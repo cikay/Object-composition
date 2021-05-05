c1d50a6c02b0f0aedf1660418cbb9aee9d8ab99f
cıkay <muzaffer123cikay@gmail.com>
2021-05-06 02:06:04 +0300

    component function wrapped a function instead of object

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-const canCast = (state) => ({
-  cast(spell) {
-    const { publicState } = state
-    console.log(`${publicState.name} casts ${spell}`)
-    publicState.mana--
-    console.log(publicState.mana)
-  },
-})
+const getCast = (state) => (spell) => {
+  const { publicState } = state
+  console.log(`${publicState.name} casts ${spell}`)
+  publicState.mana--
+  console.log(publicState.mana)
+}
 
-const canOtherThing = (state) => ({
-  doOtherThing() {
-    const { publicState, privateState } = state
-    console.log('I am doing other stuff')
-    console.log('and this is state that I access', state)
-    console.log('before ', privateState.privateProp)
-    privateState.privateProp = 'changed in doOtherThing method'
-    console.log('after ', privateState.privateProp)
-  },
-})
+const getDoOtherThing = (state) => () => {
+  const { privateState } = state
+  console.log('I am doing other stuff')
+  console.log('and this is state that I access', state)
+  console.log('before ', privateState.privateProp)
+  privateState.privateProp = 'changed in doOtherThing method'
+  console.log('after ', privateState.privateProp)
+}
 
-const privateMethod = Symbol('privateMethod')
-const canPrivateThing = (state) => ({
-  [privateMethod]() {
-    const { privateState } = state
-    console.log('I am private method')
-    console.log('before ', privateState.privateProp)
-    privateState.privateProp = 'changed in privateMethod method'
-    console.log('after ', privateState.privateProp)
-  },
-})
+const getPrivateMethod = (state) => () => {
+  const { privateState } = state
+  console.log('I am private method')
+  console.log('before ', privateState.privateProp)
+  privateState.privateProp = 'changed in privateMethod method'
+  console.log('after ', privateState.privateProp)
+}
 
-const canFight = (state) => ({
-  fight() {
-    const { publicState } = state
-    console.log(`${publicState.name} slashes at the foe`)
-    publicState.stamina--
-    const privateThingObj = canPrivateThing(state)
-    privateThingObj[privateMethod]()
-  },
-})
+const getFight = (state) => () => {
+  const { publicState } = state
+  console.log(`${publicState.name} slashes at the foe`)
+  publicState.stamina--
+  const privateMethod = getPrivateMethod(state)
+  privateMethod()
+}
 
 const createFighter = (name) => {
   const publicState = {
@@ -54,16 +45,19 @@ const createFighter = (name) => {
     publicState,
     privateState,
   }
+  const privateMethod = Symbol('privateMethod')
 
-  return Object.assign(
-    {},
-    publicState,
-    canFight(state),
-    canOtherThing(state),
-    canPrivateThing(state)
-  )
+  const behavior = {
+    fight: getFight(state),
+    doOtherThing: getDoOtherThing(state),
+    cast: getCast(state),
+    [privateMethod]: getPrivateMethod(state),
+  }
+
+  return Object.assign({}, publicState, behavior)
 }
 
 const fighter = createFighter('Muzaffer')
 fighter.fight()
 fighter.doOtherThing()
+console.log(fighter)
